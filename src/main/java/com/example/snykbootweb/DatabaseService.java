fbf550b5502988a32bd3951d355ede3fbcc89960
thnbih <22520136@gm.uit.edu.vn>
2025-04-11 14:12:12 +0700

    test snyk

--- src/main/java/com/example/snykbootweb/DatabaseService.java
+++ src/main/java/com/example/snykbootweb/DatabaseService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 public class DatabaseService {
     private String userName = "admin";
     private String password = "shwhehe67whd!";
-
+//ok
     public String getPassword () {
         return password;
     }
